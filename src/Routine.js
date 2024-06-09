a0455232d3948b53d3d492ba828830d51fbf8d4c
KIMOUGINOT <mouginotkilian@gmail.com>
2024-06-09 18:55:54 +0200

    icons

--- src/Routine.js
+++ src/Routine.js
@@ -7,7 +7,13 @@ const Routine = ({ name, data }) => {
             <div className="title-container">
                 <h2 className="routine-title">{name}</h2>
                 <button className="edit-button">
-                    <img src={process.env.PUBLIC_URL + '/editer.png'} alt="Editer icon" className="edit-icon"/>
+                    <img src={process.env.PUBLIC_URL + '/editer.png'} alt="Editer icon" className="icon"/>
+                </button>
+                <button className="button">
+                    <img src={process.env.PUBLIC_URL + '/supprimer.png'} alt="Eraser icon" className="icon"/>
+                </button>
+                <button className="button">
+                    <img src={process.env.PUBLIC_URL + '/bouton-de-lecture.png'} alt="Read icon" className="icon"/>
                 </button>
             </div>
             {data.map((exo, index) => (
